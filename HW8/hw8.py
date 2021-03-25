226f5204b190fe299478be5efa3da38ebe496ee2
werekrol <werekrol@gmail.com>
2021-03-25 21:59:37 +0300

    initial commit

--- HW8/hw8.py
+++ HW8/hw8.py
@@ -1,18 +1,3 @@
-# import os
-# list_ = []
-# list_name = os.listdir('sorted')
-
-# for i in list_name:
-#     with open(f'sorted/{i}', encoding='utf-8') as f:
-#         lines = f.readlines()
-#         line = [i + '\n'] + [str(len(lines)) + '\n'] + lines
-#         list_.append(line)
-#     text = ' '.join(sum((sorted(list_, key=len)), [])
-
-# # with open('all.txt', 'w', encoding='utf-8') as m:
-# #     m.write('text')  
-
-
 import os
 ls = [i for i in os.listdir() if i.endswith('.txt')]
 
